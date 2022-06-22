6cd407e5fb5d01515a0fa1ab017eeed75285453e
Sina <36819672+cnankh@users.noreply.github.com>
2022-06-22 09:38:29 +0430

    update: inject Api model to BaseRemoteDataSource

--- app/src/main/java/com/example/stormy/core/base/BaseRemoteDataSource.kt
+++ app/src/main/java/com/example/stormy/core/base/BaseRemoteDataSource.kt
@@ -1,9 +1,15 @@
 package com.example.stormy.core.base
 
+import com.example.stormy.core.di.Api
 import io.ktor.client.*
 import javax.inject.Inject
 
 abstract class BaseRemoteDataSource {
+
+
+    @Inject
+    lateinit var api: Api
+
     @Inject
     lateinit var client: HttpClient
 }
